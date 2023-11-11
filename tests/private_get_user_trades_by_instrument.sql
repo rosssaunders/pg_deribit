f95a52f4829020de4a2d2901f422755b5d15e7a2
Ross Saunders <rosssaunders@outlook.com>
2023-11-11 23:18:40 +0000

    fixes

--- tests/private_get_user_trades_by_instrument.sql
+++ tests/private_get_user_trades_by_instrument.sql
@@ -1,30 +1,24 @@
 with recursive trades as (
-    select a.result
+    select a.*
     from deribit.private_get_user_trades_by_instrument(
-              deribit.private_get_user_trades_by_instrument_request_builder(
-                      instrument_name := 'ETH-PERPETUAL',
-                      count := 1,
-                      start_timestamp := 0,
-                      sorting := 'asc'
-        )
-    ) a
+          instrument_name := 'ETH-PERPETUAL',
+          count := 1,
+          start_timestamp := 0,
+          sorting := 'asc'
+       ) a
     union all
-    select b.result
+    select b.*
     from trades t
     cross join deribit.private_get_user_trades_by_instrument(
-              deribit.private_get_user_trades_by_instrument_request_builder(
-                      instrument_name := 'ETH-PERPETUAL',
-                      sorting := 'asc',
-                      count := 100,
-                      start_seq := (select max(v.trade_seq) from unnest((t.result).trades) v) + 1
-        )
-    ) b
-    where (t.result).has_more
+          instrument_name := 'ETH-PERPETUAL',
+          sorting := 'asc',
+          count := 100,
+          start_seq := (select max(v.trade_seq) from unnest((t).trades) v) + 1
+             ) b
+    where t.has_more
 )
-select ((a.t)::deribit.private_get_user_trades_by_instrument_trade).*
-from (
-    select unnest((t.result).trades) as t
-    from trades t
-) a;
+select ((a.t)::deribit.private_get_user_trades_by_instrument_response_trade).*
+from (select unnest((t).trades) as t
+      from trades t) a;
 
 
